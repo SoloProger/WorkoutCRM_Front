fde17956965e07e989c341fba6a81eec8cc6c15c
SoloProger <solo.proger@gmail.com>
2022-07-08 00:25:09 +0300

    start work on calendar

--- src/app/shared/calendar/calendar.component.ts
+++ src/app/shared/calendar/calendar.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+interface CalendarColumn {
+  field: string;
+  header: string;
+}
+
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
-  styleUrls: ['./calendar.component.scss']
+  styleUrls: ['./calendar.component.scss'],
 })
 export class CalendarComponent implements OnInit {
+  public columns: CalendarColumn[] = [
+    { field: 'monday', header: 'Понедельник' },
+    { field: 'monday', header: 'Вторник' },
+    { field: 'monday', header: 'Среда' },
+    { field: 'monday', header: 'Четверг' },
+    { field: 'monday', header: 'Пятница' },
+    { field: 'monday', header: 'Суббота' },
+    { field: 'monday', header: 'Воскресенье' },
+  ];
 
-  constructor() { }
+  public count = [1, 2, 3, 4, 5, 6, 7];
+  public count2 = [8, 9, 10, 11, 12, 13, 14];
+  public count3 = [15, 16, 17, 18, 19, 20, 21];
+  public count4 = [22, 23, 24, 25, 26, 27, 28];
+  public count5 = [29, 30, 31, 1, 2, 3, 4];
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
